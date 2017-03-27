c8333ae6ca76d9d85e7b1a338ac37389018e4734
asnayak9 <nayak.abhishek462@gmail.com>
2017-03-27 11:26:24 +0530

    update  security code

--- src/main/java/com/spoc/controller/LoginController.java
+++ src/main/java/com/spoc/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.spoc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -35,7 +36,7 @@ public class LoginController {
 	{		
 		System.out.println(" user name "+userForm.getUserName());
 		System.out.println(" user password "+userForm.getPassword());
-		
+		 Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		return "home-page";
 	}
 	
--- src/main/java/com/spoc/validation/CustomAuthenticationProviderService.java
+++ src/main/java/com/spoc/validation/CustomAuthenticationProviderService.java
@@ -33,6 +33,7 @@ public class CustomAuthenticationProviderService implements AuthenticationProvid
   @Override
   public Authentication authenticate(Authentication authentication) throws AuthenticationException {
     
+	  System.out.println("************************************************* hit");
     String username = authentication.getName();
     String password = (String) authentication.getCredentials();
     UserDetails user = null;
